d748defcd986edea7eef4aa33dcea63b5c610f13
lvaolin <414906978@qq.com>
2019-08-15 23:11:59 +0800

    1

---
+++ src/main/java/hashcode/User.java
@@ -0,0 +1,20 @@
+package hashcode;
+
+public class User {
+    String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return name != null ? name.equals(user.name) : user.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+}
