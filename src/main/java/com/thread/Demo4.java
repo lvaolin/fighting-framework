822f3b1983fe469c75790c287030a175713d495f
lval <lvaolin@foresee.com.cn>
2019-09-11 14:16:09 +0800

    多线程

---
+++ src/main/java/com/thread/Demo4.java
@@ -0,0 +1,69 @@
+package com.thread;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 交替输出 字母和数字----------------------------
+ */
+public class Demo4 {
+
+        static Thread t1 = null;
+        static Thread t2 = null;
+        static Lock lock = new ReentrantLock();
+        public static void main(String[] args) {
+
+           t1 =  new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        lock.lock();
+                        print1();
+                        lock.unlock();
+                        synchronized (Demo4.class){
+                            Demo4.class.notify();
+                            Demo4.class.wait();
+                        }
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+         t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        lock.lock();
+                        print2();
+                        lock.unlock();
+                        synchronized (Demo4.class){
+                            Demo4.class.notify();
+                            Demo4.class.wait();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+
+    public static void print1() throws InterruptedException {
+                System.out.println("a");
+    }
+    public static void print2() throws InterruptedException {
+            System.out.println("1");
+    }
+
+}
---
+++ src/main/java/com/thread/Demo6.java
@@ -0,0 +1,64 @@
+package com.thread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 交替输出 字母和数字----------------------------
+ */
+public class Demo6 {
+
+        static Thread t1 = null;
+        static Thread t2 = null;
+        static Lock lock = new ReentrantLock();
+        public static void main(String[] args) {
+
+           t1 =  new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        synchronized (Demo6.class){
+                            print1();
+                            Demo6.class.notify();
+                            Demo6.class.wait();
+                        }
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+         t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        synchronized (Demo6.class){
+                            print2();
+                            Demo6.class.notify();
+                            Demo6.class.wait();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+
+    public static void print1() throws InterruptedException {
+                System.out.println("a");
+    }
+    public static void print2() throws InterruptedException {
+            System.out.println("1");
+    }
+
+}
