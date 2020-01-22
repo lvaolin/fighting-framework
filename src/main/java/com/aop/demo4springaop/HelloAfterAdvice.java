8cf98f702c2656d8a65391386bd69270cb9d2767
lvaolin <lvaolin@foresee.com.cn>
2020-01-22 17:01:28 +0800

    Spring  AOP测试

---
+++ src/main/java/com/aop/demo4springaop/HelloAfterAdvice.java
@@ -0,0 +1,18 @@
+package com.aop.demo4springaop;
+
+import org.springframework.aop.AfterReturningAdvice;
+
+import java.lang.reflect.Method;
+
+/**
+ * 实现后置增强接口
+ *
+ * @author lvaolin
+ * @create 2020/1/22 3:12 PM
+ */
+public class HelloAfterAdvice implements AfterReturningAdvice {
+    @Override
+    public void afterReturning(Object o, Method method, Object[] objects, Object o1) throws Throwable {
+        System.out.println("after hello");
+    }
+}
---
+++ src/main/java/com/aop/demo4springaop/IHello.java
@@ -0,0 +1,11 @@
+package com.aop.demo4springaop;
+
+/**
+ * @author lvaolin
+ * @create 2020/1/22  3:15 PM
+ */
+
+public interface IHello {
+
+    void  say(String name);
+}
