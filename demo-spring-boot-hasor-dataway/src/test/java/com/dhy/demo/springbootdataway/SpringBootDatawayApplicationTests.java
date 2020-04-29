5612323f71784ab52d7f5c0cb6cbd1e1503ea96b
lvaolin <lvaolin@foresee.com.cn>
2020-04-29 19:38:03 +0800

    hasor-dataway

---
+++ demo-spring-boot-hasor-dataway/src/test/java/com/dhy/demo/springbootdataway/SpringBootDatawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhy.demo.springbootdataway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
