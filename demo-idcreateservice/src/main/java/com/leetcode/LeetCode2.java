04820aa4377b3cfc2080a35d7f210cef9fd03764
lvaolin <lvaolin@foresee.com.cn>
2020-05-19 10:23:15 +0800

    判断回文数字

--- demo-idcreateservice/src/main/java/com/leetcode/LeetCode2.java
+++ demo-idcreateservice/src/main/java/com/leetcode/LeetCode2.java
@@ -26,14 +26,28 @@ public class LeetCode2 {
         //边界判断
         if (x < 0) return false;
         int div = 1;
-        //
-        while (x / div >= 10) div *= 10;
+        //x=123454321  div=10000000
+        //找到相同位数的除数，神奇的地方在 除它可以得到最左位，余它可以得到最右位
+        while (x / div >= 10) {
+            div =div * 10;
+        }
+        //123454321
+        //2345432
+        //34543
+        //454
+        //5
+        //利用的特性 取一个数的最左一位用 除法，取一个数的最右一位用求余法
+        //对比最左位与最右位
+        //去除最左位与最右位  用先余法后除法，真神奇
+        //接着重复，只要出现 最左位与最右位 不相等 则退出
         while (x > 0) {
             int left = x / div;
             int right = x % 10;
-            if (left != right) return false;
+            if (left != right) {
+                return false;
+            }
             x = (x % div) / 10;
-            div /= 100;
+            div = div/100;
         }
         return true;
     }
@@ -41,6 +55,19 @@ public class LeetCode2 {
 
     public static void main(String[] args) {
         LeetCode2 leetCode2 = new LeetCode2();
-        System.out.println(leetCode2.isPalindrome(103301));
+        System.out.println(leetCode2.isPalindrome2(123454321));
+        //leetCode2.fanzhuan(123456);
+    }
+
+    public void fanzhuan(int x){
+        int div = 1;
+        while (x / div >= 1){
+            System.out.println(x%div);
+            div *= 10;
+        }
+
+
+        //System.out.println(x%100);
+        //System.out.println(x%1000);
     }
 }
