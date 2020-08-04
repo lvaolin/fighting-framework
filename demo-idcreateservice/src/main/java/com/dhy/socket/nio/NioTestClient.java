2895b9b848fbba3361d32e66a1800849fd20f1d5
dhy <414906978@qq.com>
2020-08-04 22:08:09 +0800

    NIO test

--- demo-idcreateservice/src/main/java/com/dhy/socket/nio/NioTestClient.java
+++ demo-idcreateservice/src/main/java/com/dhy/socket/nio/NioTestClient.java
@@ -1,5 +1,8 @@
 package com.dhy.socket.nio;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Java NIO测试 客户端
  *
@@ -7,4 +10,40 @@ package com.dhy.socket.nio;
  * @create 2019/11/11 8:17 PM
  */
 public class NioTestClient {
+
+    public static void main(String[] args) {
+
+        try {
+            Socket client = new Socket("127.0.0.1",9090);
+
+            client.setSendBufferSize(20);
+            client.setTcpNoDelay(true);
+            OutputStream out = client.getOutputStream();
+
+            OutputStream fileout = new FileOutputStream(new File("c:/downloadlimit.txt"));
+            InputStream serverIn = client.getInputStream();
+
+            InputStream in = System.in;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            while(true){
+                String line = reader.readLine();
+                if(line != null ){
+                    byte[] bb = line.getBytes();
+                    out.write(bb);
+                }
+                if ("download".equals(line)) {
+                    //serverIn.transferTo(fileout);
+                }else{
+                    int len =0;
+                    while ( (len = serverIn.read())>0){
+                        System.out.println(serverIn.readNBytes(len).toString());
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
