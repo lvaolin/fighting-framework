9445fc740e2325368fa5c90efc203955b5543afd
dhy <414906978@qq.com>
2020-09-13 22:38:53 +0800

    荷兰国旗问题

--- demo-idcreateservice/src/main/java/com/dhy/sort/Netherlands.java
+++ demo-idcreateservice/src/main/java/com/dhy/sort/Netherlands.java
@@ -1,6 +1,9 @@
 package com.dhy.sort;
 
 /**
+ *
+ * 荷兰国旗问题
+ *
  * 给定一个整数数组，给定一个值K，这个值在原数组中一定存在，
  * 要求把数组中小于K的元素放到数组的左边，大于K的元素放到数组的右边，
  * 等于K的元素放到数组的中间，最终返回一个整数数组，其中只有两个值，
@@ -14,6 +17,13 @@ package com.dhy.sort;
  */
 public class Netherlands {
 
+
+    public static void main(String[] args) {
+        int[] arr = {1,6,3,5,6,7,6,9,2,4,6,8};
+        Netherlands netherlands = new Netherlands();
+        netherlands.netherlandsFlag(arr,0,arr.length-1,6);
+    }
+
     /**
      *
      * @param arr
@@ -22,8 +32,31 @@ public class Netherlands {
      * @param target
      */
 
-    public void netherlandsFlag(int[] arr,int L,int R,int target){
-
+    public int[] netherlandsFlag(int[] arr,int L,int R,int target){
+        int less = L - 1;
+        int more = R + 1;
+        while(L < more) {
+            if(arr[L] < target) {
+                swap(arr, ++less, L++);
+            } else if (arr[L] > target) {
+                swap(arr, --more, L);
+            } else {
+                L++;
+            }
+        }
+        print(arr);
+        return new int[] { less + 1, more - 1 };
+    }
+    public  void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
+
+    void print(int[] arr){
+        for (int i : arr) {
+            System.out.print(i);
+        }
+    }
 }
