cb017ef3169b67e5706f616007f28610b8522425
dhy <414906978@qq.com>
2020-10-06 17:49:43 +0800

    限流滑动窗口算法

--- demo-idcreateservice/src/main/java/com/dhy/leetcode/sentinel/Main.java
+++ demo-idcreateservice/src/main/java/com/dhy/leetcode/sentinel/Main.java
@@ -7,32 +7,18 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    //当前窗口请求数量
-    public static  final AtomicInteger requestCount = new AtomicInteger();
-    //能否通过的标志
-    public static volatile boolean pass = true;
+
 
     public static void main(String[] args) {
-        //定时刷新窗口数据
-        Executors.newScheduledThreadPool(1).scheduleWithFixedDelay(new Runnable() {
-            @Override
-            public void run() {
-                if (RateLimitFactory.isOverLimit("http://xxx.com/a/b", requestCount.intValue())) {
-                    pass = false;
-                } else {
-                    pass = true;
-                }
-                requestCount.set(0);
-            }
-        }, 0L, 1000 / RateLimitFactory.defaultWinSize, TimeUnit.MILLISECONDS);
+
 
         for (int i = 0; i < 2; i++) {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     while (true) {
-                        if (pass) {
-                            requestCount.addAndGet(RandomUtils.nextInt(100));
+                        if (RateLimitFactory.pass) {
+                            RateLimitFactory.requestCount.addAndGet(RandomUtils.nextInt(100));
                             System.out.println(Thread.currentThread().getName()+"  passed");
                         }else{
                             System.out.println(Thread.currentThread().getName()+" no passed");
