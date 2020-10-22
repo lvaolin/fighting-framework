e6432a7e60142dc44220e0eef47f5148f6ec7930
lvaolin <lvaolin@foresee.com.cn>
2020-10-22 16:42:53 +0800

    优化

--- demo-idcreateservice/src/main/java/com/dhy/util/peakcut/impl/DefaultExecutorServiceHandler.java
+++ demo-idcreateservice/src/main/java/com/dhy/util/peakcut/impl/DefaultExecutorServiceHandler.java
@@ -6,7 +6,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class DefaultExecutorServiceHandler implements ExecutorServiceHandler {
-    private int defaultPoolSize = 100;
+    /**
+     * @todo 高峰期可上去，低谷期可下来
+     */
+    private int defaultPoolSize = 50;
     @Override
     public ExecutorService createExecutorService(Integer poolSize) {
         ExecutorService executorService = Executors.newFixedThreadPool(poolSize!=null?poolSize.intValue():defaultPoolSize);
