a8cb03a5e6949a519b20027a2b7649793b7be8d3
dhy <414906978@qq.com>
2020-11-03 21:37:40 +0800

    embeded tomcat

--- dubbo-rpc/rpc-dubbo/src/main/java/com/dhy/dubbo/protocol/http/HttpServer.java
+++ dubbo-rpc/rpc-dubbo/src/main/java/com/dhy/dubbo/protocol/http/HttpServer.java
@@ -1,6 +1,5 @@
 package com.dhy.dubbo.protocol.http;
 
-import org.apache.catalina.Host;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
@@ -10,19 +9,17 @@ public class HttpServer {
 
     public static void main(String[] args) {
         HttpServer httpServer = new HttpServer();
-        httpServer.start("loalhost",8080);
+        httpServer.start("loalhost", 8080);
     }
 
     Tomcat tomcat = new Tomcat();
-
-    public void start(String hostname,Integer port){
+    public void start(String hostname, Integer port) {
         tomcat.setBaseDir(".");
         tomcat.setHostname(hostname);
         tomcat.setPort(port);
         tomcat.getConnector();
-        tomcat.setBaseDir("./tomcat");
-        tomcat.addWebapp("/v1", new File("./tomcat/webapps/v1").getAbsolutePath());
-
+        //单独指定某个app的地址，以及上下文路径
+        tomcat.addWebapp("/v1", new File("./webapps/v1").getAbsolutePath());
         try {
             tomcat.start();
             tomcat.getServer().await();
@@ -32,7 +29,7 @@ public class HttpServer {
 
     }
 
-    public void stop(){
+    public void stop() {
 
     }
 }
