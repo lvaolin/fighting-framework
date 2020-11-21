eff7eee98a868db49538fafd431cbd599cc76914
dhy <414906978@qq.com>
2020-11-21 22:08:37 +0800

    web flux demo

---
+++ spring-webflux/src/main/java/com/dhy/springwebflux/HelloWebFlux.java
@@ -0,0 +1,20 @@
+package com.dhy.springwebflux;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class HelloWebFlux {
+    @Bean
+    public RouterFunction<ServerResponse> hello(HelloHandler handler) {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/hello")
+                                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
+                        handler::hello);
+    }
+}
