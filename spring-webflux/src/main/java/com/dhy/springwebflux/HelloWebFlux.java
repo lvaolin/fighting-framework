488b81ef99513c22ef8af99845b81a12b9c4247a
dhy <414906978@qq.com>
2020-11-21 22:13:57 +0800

    web flux demo

--- spring-webflux/src/main/java/com/dhy/springwebflux/HelloWebFlux.java
+++ spring-webflux/src/main/java/com/dhy/springwebflux/HelloWebFlux.java
@@ -3,10 +3,9 @@ package com.dhy.springwebflux;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class HelloWebFlux {
@@ -16,5 +15,21 @@ public class HelloWebFlux {
                 .route(RequestPredicates.GET("/hello")
                                 .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
                         handler::hello);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> helloword(HelloHandler handler) {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/helloword")
+                                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
+                        new HandlerFunction<ServerResponse>() {
+                            @Override
+                            public Mono<ServerResponse> handle(ServerRequest serverRequest) {
+                                Mono<ServerResponse> body = ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+                                        .body(BodyInserters.fromObject("Hello, webflux !"));
+
+                                return body;
+                            }
+                        });
     }
 }
