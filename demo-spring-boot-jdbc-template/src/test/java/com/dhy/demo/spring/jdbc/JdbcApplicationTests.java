cefb3dde16b20007d4ca490f9692d68e119aa665
dhy <414906978@qq.com>
2020-11-21 11:07:52 +0800

    手动装配 druidDataSource 属性

--- demo-spring-boot-jdbc-template/src/test/java/com/dhy/demo/spring/jdbc/JdbcApplicationTests.java
+++ demo-spring-boot-jdbc-template/src/test/java/com/dhy/demo/spring/jdbc/JdbcApplicationTests.java
@@ -1,13 +1,25 @@
 package com.dhy.demo.spring.jdbc;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class JdbcApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        //默认是 hikari 连接池，可以换成  druid试试
+        System.out.println(dataSource.getClass());
+        System.out.println(dataSource.getConnection());
+        DruidDataSource druidDataSource = (DruidDataSource)dataSource;
+        System.out.println(druidDataSource.getInitialSize());
+        System.out.println(druidDataSource.getMaxActive());
     }
 
 }
