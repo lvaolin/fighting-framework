2a43019899b4f69597fda9c38706c514266d7a05
lvaolin <lvaolin@foresee.com.cn>
2021-02-20 16:44:52 +0800

    内存队列LinkedListBlockingQueue 的应用：支持背压检测

---
+++ fighting-core/src/main/java/com/dhy/queue/MyQueue.java
@@ -0,0 +1,21 @@
+package com.dhy.queue;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.concurrent.*;
+
+/**
+ * @Project fighting
+ * @Description 队列
+ * @Author lvaolin
+ * @Date 2021/2/20 3:56 下午
+ */
+public class MyQueue {
+    public static BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+    public static int nThreads = 10;
+    public static ExecutorService fixedExecutorService = new ThreadPoolExecutor(nThreads, nThreads,
+            0L, TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<Runnable>(100));
+    public static ExecutorService cachedExecutorService = Executors.newCachedThreadPool();
+}
---
+++ fighting-core/src/main/java/com/dhy/queue/QueueTest.java
@@ -0,0 +1,75 @@
+package com.dhy.queue;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Project fighting
+ * @Description 主要用途描述
+ * @Author lvaolin
+ * @Date 2021/2/20 3:55 下午
+ */
+public class QueueTest {
+
+
+    public static void main(String[] args) {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ExecutorService executorService = MyQueue.cachedExecutorService;
+                    final AtomicInteger count = new AtomicInteger(0);
+                    while (true){
+                        String take = MyQueue.queue.take();
+
+                        while (count.intValue()>100){
+                            System.out.println("并发大于100时暂停");
+                            TimeUnit.MILLISECONDS.sleep(500);
+                        }
+                        System.out.println("继续消费");
+                        Future<?> submit = executorService.submit(new Runnable() {
+                            @Override
+                            public void run() {
+                                count.incrementAndGet();
+                                System.out.println("consumer：" + take);
+                                try {
+                                    TimeUnit.SECONDS.sleep(1);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                count.decrementAndGet();
+                            }
+                        });
+
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+
+        for (int i = 0; i <1000 ; i++) {
+            try {
+                MyQueue.queue.put(UUID.randomUUID().toString());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+//        try {
+//            thread.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println("end");
+
+    }
+
+}
