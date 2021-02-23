69ba0448a26fb945d45c5752d99cba93c8d10320
lvaolin <lvaolin@foresee.com.cn>
2021-02-23 18:28:29 +0800

    new Timer().schedule 在宿主对象没有任何引用的情况下 会自动退出吗

---
+++ fighting-core/src/main/java/com/dhy/timetask/TestMain.java
@@ -0,0 +1,21 @@
+package com.dhy.timetask;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Project fighting
+ * @Description 主要用途描述
+ * @Author lvaolin
+ * @Date 2021/2/23 6:02 下午
+ */
+public class TestMain {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        while (true){
+            LimiterConfigMgr limiterConfigMgr = new LimiterConfigMgr();
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+}
