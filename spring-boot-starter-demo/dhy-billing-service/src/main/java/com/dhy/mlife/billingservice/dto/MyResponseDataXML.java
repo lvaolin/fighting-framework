dcc0ae99301442a51611483f96436c2a9f07e31d
lvaolin <lvaolin@foresee.com.cn>
2022-05-09 22:29:16 +0800

    实现xml格式响应

---
+++ spring-boot-starter-demo/dhy-billing-service/src/main/java/com/dhy/mlife/billingservice/dto/MyResponseDataXML.java
@@ -0,0 +1,45 @@
+package com.dhy.mlife.billingservice.dto;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@JacksonXmlRootElement
+public class MyResponseDataXML<T> implements Serializable {
+    private Head head = new Head("0","success");
+    private T body;
+    public MyResponseDataXML(T bizData){
+        this.body = bizData;
+    }
+
+    public MyResponseDataXML(String code, String msg){
+        this.head = new Head(code,msg);
+    }
+
+    public static class Head{
+        public Head(String code,String msg){
+            this.code = code;
+            this.msg = msg;
+        }
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
+        }
+
+        private String code;
+        private String msg;
+    }
+}
