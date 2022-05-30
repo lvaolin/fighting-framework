7540168d84e13968c158b5785d86ea7d2d923fa9
lvaolin <lvaolin@foresee.com.cn>
2022-05-30 22:33:26 +0800

    tomcat 独立部署

--- spring-boot-starter-demo/dhy-billing-service/src/main/java/com/dhy/mlife/billingservice/BillingApplication.java
+++ spring-boot-starter-demo/dhy-billing-service/src/main/java/com/dhy/mlife/billingservice/BillingApplication.java
@@ -2,14 +2,20 @@ package com.dhy.mlife.billingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {
         "com.dhy.mlife.billingservice.gateway.db.impl.mapper"
 })
-public class BillingApplication {
+public class BillingApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BillingApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(BillingApplication.class, args);
     }
