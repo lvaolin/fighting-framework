af590ebe966507dd43bec7c0dcba21492cf4ca80
lvaolin <lvaolin@foresee.com.cn>
2022-11-30 22:09:44 +0800

    javaagent socket监控组件测试

--- spring-boot-mybatis/src/main/java/com/dhy/demo/spring/mybatis/MybatisApplication.java
+++ spring-boot-mybatis/src/main/java/com/dhy/demo/spring/mybatis/MybatisApplication.java
@@ -4,6 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 //@MapperScan("com.dhy.demo.spring.mybatis.infrastructure.datebase.mybatis.mapper")
 //MapperScan指定扫描包与Mapper接口类上的Mapper注解有一方存在即可
@@ -12,6 +16,23 @@ public class MybatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisApplication.class, args);
+        new Thread(()->{
+            while (true){
+                try {
+                    TimeUnit.SECONDS.sleep(120);
+                    Map<Socket,Exception> map= (Map<Socket, Exception>)Socket.class.getDeclaredField("socketMap").get(null);
+                    System.out.println("遗留Socket连接："+map.size()+"个");
+                    map.forEach((s,e)->{
+                        System.out.println(s.toString()+"---->");
+                        System.out.println(e.getMessage() + "---->");
+                        e.printStackTrace();
+                    });
+                } catch (Exception e) {
+                    //e.printStackTrace();
+                }
+
+            }
+        }).start();
     }
 
 }
