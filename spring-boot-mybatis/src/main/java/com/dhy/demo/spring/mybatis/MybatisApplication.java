df5e9b333fa3fd126bda6011b4503da7eeee0d00
lvaolin <lvaolin@foresee.com.cn>
2022-12-05 20:23:15 +0800

    agent测试

--- spring-boot-mybatis/src/main/java/com/dhy/demo/spring/mybatis/MybatisApplication.java
+++ spring-boot-mybatis/src/main/java/com/dhy/demo/spring/mybatis/MybatisApplication.java
@@ -3,7 +3,6 @@ package com.dhy.demo.spring.mybatis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import sun.nio.ch.MySocketMonitor;
 @SpringBootApplication
 //@MapperScan("com.dhy.demo.spring.mybatis.infrastructure.datebase.mybatis.mapper")
 //MapperScan指定扫描包与Mapper接口类上的Mapper注解有一方存在即可
@@ -12,7 +11,6 @@ public class MybatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisApplication.class, args);
-        MySocketMonitor.print();
     }
 
 
